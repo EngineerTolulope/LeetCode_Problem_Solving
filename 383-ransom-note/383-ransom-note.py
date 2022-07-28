2da779ed980c05e8e78030533f85b7c60b2dc9d0
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-07-28 09:22:42 -0400

    Time: 116 ms (29.01%), Space: 14.2 MB (52.43%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomSet, magazineSet = {}, {}
+        
+        for char in ransomNote:
+            ransomSet[char] = 1 + ransomSet.get(char, 0)
+            
+        for char in magazine:
+            magazineSet[char] = 1 + magazineSet.get(char, 0)
+            
+        for char in ransomSet:
+            magazine_count = magazineSet.get(char, 0)
+            if not ransomSet[char] <= magazine_count:
+                return False
+        
+        return True
+            
