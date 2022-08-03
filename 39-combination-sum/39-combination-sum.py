d279ea1bf8d6dffd3e943f0227a18204207090b0
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-02 22:39:11 -0400

    Time: 165 ms (30.55%), Space: 14.2 MB (36.16%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        combinations = []
+        
+        def depth_first_search(i, current_combo, total):
+            if total == target:
+                combinations.append(current_combo.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            current_combo.append(candidates[i])
+            depth_first_search(i, current_combo, total + current_combo[-1])
+            current_combo.pop()
+            depth_first_search(i + 1, current_combo, total)
+        
+        depth_first_search(0, [], 0)
+        return combinations
+            
+            
