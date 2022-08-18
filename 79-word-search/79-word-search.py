0c3208f2a74f480c6de48b353dbfbec7b4cd2ccf
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-18 18:31:42 -0400

    Time: 4938 ms (88.24%), Space: 13.9 MB (51.29%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,8 +1,9 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLUMNS = len(board), len(board[0])
+        visited = set()
         
-        def depth_first_search(row, column, index, visited):
+        def depth_first_search(row, column, index):
             if index == len(word):
                 return True
             if (row < 0 or column < 0 or row == ROWS or column == COLUMNS
@@ -10,15 +11,15 @@ class Solution:
                 return False
             
             visited.add((row, column))
-            result = (depth_first_search(row, column - 1, index + 1, visited) or
-                      depth_first_search(row, column + 1, index + 1, visited) or
-                      depth_first_search(row - 1, column, index + 1, visited) or
-                      depth_first_search(row + 1, column, index + 1, visited))
+            result = (depth_first_search(row, column - 1, index + 1) or
+                      depth_first_search(row, column + 1, index + 1) or
+                      depth_first_search(row - 1, column, index + 1) or
+                      depth_first_search(row + 1, column, index + 1))
             visited.remove((row, column))
             return result
         
         for row in range(ROWS):
             for column in range(COLUMNS):
-                if depth_first_search(row, column, 0, set()):
+                if depth_first_search(row, column, 0):
                     return True
         return False
