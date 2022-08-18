8fafc30aa63a883473376937cb6fc60e47e10ebf
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-18 19:43:27 -0400

    Time: 120 ms (92.50%), Space: 15.2 MB (80.92%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -20,8 +20,7 @@ class Solution:
             if s_count[s[left]] == 0:
                 s_count.pop(s[left])
             
-            left += 1
-            right += 1
+            left += 1; right += 1
             if right != s_length:
                 s_count[s[right]] = 1 + s_count.get(s[right], 0)
         
