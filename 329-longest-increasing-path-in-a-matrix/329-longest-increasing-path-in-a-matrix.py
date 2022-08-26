b1460679a7e6fb80190e442ef6bbc477280f9c03
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-26 18:52:18 -0400

    Time: 664 ms (61.22%), Space: 18.5 MB (28.91%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLUMNS = len(matrix), len(matrix[0])
+        maximum_path = {} # dynamic programming
+        
+        def depth_first_search(row, column, previous_value):
+            if (row not in range(0, ROWS) or column not in range(0, COLUMNS)
+               or matrix[row][column] <= previous_value):
+                return 0
+            if (row, column) in maximum_path:
+                return maximum_path[(row, column)]
+            
+            result = 1
+            result = max(result, 1 + depth_first_search(row - 1, column, matrix[row][column]),
+                        1 + depth_first_search(row + 1, column, matrix[row][column]),
+                        1 + depth_first_search(row, column - 1, matrix[row][column]),
+                        1 + depth_first_search(row, column + 1, matrix[row][column]))
+            maximum_path[(row, column)] = result
+            return result
+        
+        for row in range(ROWS):
+            for column in range(COLUMNS):
+                depth_first_search(row, column, -1)
+                
+        return max(maximum_path.values())
+        
+        
