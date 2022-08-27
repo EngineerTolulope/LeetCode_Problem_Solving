20d956cb35ddfa025298d6145dde5c7e7e62ff16
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-26 22:27:19 -0400

    Time: 1279 ms (11.26%), Space: 60.8 MB (7.81%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        ROWS, COLUMNS = len(matrix), len(matrix[0])
+        max_length = {} # stores the max square length
+        def depth_first_search(row, column):
+            if row == ROWS or column == COLUMNS:
+                return 0
+            if (row, column) not in max_length:
+                right = depth_first_search(row, column + 1)
+                down = depth_first_search(row + 1, column)
+                diagonal = depth_first_search(row + 1, column + 1)
+                
+                max_length[(row, column)] = 0
+                if matrix[row][column] != '0':
+                    max_length[(row, column)] = 1 + min(right, down, diagonal)
+            return max_length[(row, column)]
+        
+        depth_first_search(0, 0)
+        return max(max_length.values()) ** 2
