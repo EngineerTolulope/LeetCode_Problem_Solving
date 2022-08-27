38a05462aa4905217426f1b3508e84adeb039f5d
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-26 22:27:52 -0400

    Time: 1528 ms (6.45%), Space: 60.9 MB (7.81%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -11,7 +11,7 @@ class Solution:
                 diagonal = depth_first_search(row + 1, column + 1)
                 
                 max_length[(row, column)] = 0
-                if matrix[row][column] != '0':
+                if matrix[row][column] == '1':
                     max_length[(row, column)] = 1 + min(right, down, diagonal)
             return max_length[(row, column)]
         
