cda2566857101fb9adcb82b83c787e344c277fe4
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-28 13:42:08 -0400

    Time: 66 ms (15.98%), Space: 13.8 MB (49.29%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,15 +1,24 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        num = list("{:032b}".format(n))
-        left, right = 0, len(num) - 1
+        result = 0
+        
+        for i in range(32):
+            current_bit = (n >> i) & 1
+            result |= (current_bit << (31 - i))
+        return result
+        
+        
+#         # convert to string then list then integer
+#         num = list("{:032b}".format(n))
+#         left, right = 0, len(num) - 1
 
-        while left <= right:
-            if num[left] == num[right]:
-                left += 1
-                right -= 1
-                continue
-            num[left], num[right] = num[right], num[left]
-            left += 1
-            right -= 1
-        num = "".join(num)
-        return int(num, 2)
+#         while left <= right:
+#             if num[left] == num[right]:
+#                 left += 1
+#                 right -= 1
+#                 continue
+#             num[left], num[right] = num[right], num[left]
+#             left += 1
+#             right -= 1
+#         num = "".join(num)
+#         return int(num, 2)
