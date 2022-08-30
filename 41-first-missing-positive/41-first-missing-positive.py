3915ff783a27680e08dd90aecb6e7b189988de59
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-08-29 20:32:21 -0400

    Time: 373 ms (89.08%), Space: 27.3 MB (89.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -14,7 +14,7 @@ class Solution:
         for i in range(1, len(nums) + 1):
             if nums[i - 1] > 0:
                 return i
-        return len(nums) + 1
+        return default_value
         
         
                 
