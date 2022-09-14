80ccc16febafd7b9305dbd5510c1aac5e549bc48
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-14 09:18:59 -0400

    Time: 97 ms (83.64%), Space: 14.9 MB (17.54%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        
+        adjacents = {source: [] for source, _ in tickets}
+        for source, destination in tickets:
+            adjacents[source].append(destination)
+            
+        result = ['JFK']
+        def backtracking(source):
+            if len(result) == len(tickets) + 1:
+                return True
+            if source not in adjacents:
+                return False
+            
+            temp = adjacents[source].copy()
+            for i, next_location in enumerate(temp):
+                adjacents[source].pop(i)
+                result.append(next_location)
+                if backtracking(next_location):
+                    return True
+                
+                adjacents[source].insert(i, next_location)
+                result.pop()
+            return False
+        
+        backtracking('JFK')
+        return result
+            
