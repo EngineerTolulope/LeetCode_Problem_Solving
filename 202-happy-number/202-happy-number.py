002d8e8f975e729720605e42ffead00089a3e147
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-16 10:06:25 -0400

    Time: 35 ms (94.49%), Space: 13.9 MB (61.54%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+        
+        while n not in visited:
+            visited.add(n)
+            n = self.get_sum_of_squares(n)
+            
+            if n == 1:
+                return True
+        return False
+    
+    def get_sum_of_squares(self, num):
+        result = 0
+        
+        while num:
+            digit = num % 10
+            result += digit * digit
+            num //= 10
+        return result
+            
