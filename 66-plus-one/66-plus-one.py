9e3d4f08416606cafae91f504d501a50f3b41d16
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-18 13:14:27 -0400

    Time: 60 ms (32.36%), Space: 13.9 MB (12.20%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,6 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits = [str(digit) for digit in digits]
-        digits = list(str(int(''.join(digits)) + 1))
-        digits = [int(digit) for digit in digits]
-        return digits
+        continue_ = True
+        i = len(digits) - 1
+        
+        while continue_:
+            if i >= 0:
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    continue_ = False
+            else:
+                digits = [1] + digits
+                continue_ = False
+            i -= 1
+        return digits 
+    
+    
+        # digits = [str(digit) for digit in digits]
+        # digits = list(str(int(''.join(digits)) + 1))
+        # digits = [int(digit) for digit in digits]
+        # return digits
