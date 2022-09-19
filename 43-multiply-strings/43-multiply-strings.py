e2733c456d71883ea0a023832d072574999cf33b
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-18 22:24:15 -0400

    Time: 79 ms (52.04%), Space: 13.8 MB (73.45%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,24 +1,19 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0":
-            return "0"
+        if '0' in [num1, num2]:
+            return '0'
         
-        int1 = 0
-        int2 = 0
+        num1_int, num2_int = 0, 0
+        for digit in num1:
+            num1_int = num1_int * 10 + ord(digit) - ord('0')
+        for digit in num2:
+            num2_int = num2_int * 10 + ord(digit) - ord('0')
         
-        # convert string to int
-        for n in num1:
-            int1 = int1 * 10 + (ord(n) - ord('0'))
-        for n in num2:
-            int2 = int2 * 10 + (ord(n) - ord('0'))
-        
-        int_res = int1 * int2
-        
-        res = ""
-        
-        while int_res:
-            remainder = int_res % 10
-            res = res + str(remainder)
-            int_res = int_res // 10
-        
-        return res[::-1]
+        num1_num2 = num1_int * num2_int
+        result = ''
+        while num1_num2:
+            digit = num1_num2 % 10
+            result += str(digit)
+            num1_num2 //= 10
+            
+        return result[::-1]
