34d196ac52e26875e511acb35188a19acf0038f9
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-21 12:10:05 -0400

    Time: 661 ms (23.61%), Space: 17.8 MB (61.39%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -3,6 +3,7 @@ class Solution:
         count = [0] * 60
         result = 0
         for time in times:
-            result += count[-time % 60]
-            count[time % 60] += 1
+            remainder = time % 60
+            result += count[60 - remainder] if remainder > 0 else count[0]
+            count[remainder] += 1
         return result
