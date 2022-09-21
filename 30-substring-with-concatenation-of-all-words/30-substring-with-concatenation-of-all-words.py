cfcc5fafc11793abbc4dba96644916d2a23c9bdf
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-20 20:27:54 -0400

    Time: 64 ms (99.35%), Space: 14.3 MB (33.27%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -1,28 +1,34 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if not s or not words:
-            return []
-        
-        word_count = {}
-        word_length = len(words[0])
-        result = []
-        words_length = len(words) * word_length
-        
+        word_len = len(words[0])
+        ori_word_dict = defaultdict(int)
+		
         for word in words:
-            word_count[word] = 1 + word_count.get(word, 0)
-            
-        for left in range(len(s) - word_length + 1):
-            words_seen = {}
-            for right in range(len(words)):
-                word_index = left + right * word_length
-                temp_word = s[word_index:word_index + word_length]
-                
-                if temp_word not in word_count:
-                    break
-                    
-                words_seen[temp_word] = words_seen.get(temp_word, 0) + 1
-                if words_seen[temp_word] > word_count[temp_word]:
-                    break
-            if words_seen == word_count:
-                result.append(left)
+            ori_word_dict[word] += 1
+        
+        all_word_len = len(words) * word_len
+        result = []
+        for i in range(word_len):
+            queue = deque()
+            word_dict = ori_word_dict.copy()
+            for j in range(i, len(s) - word_len + 1, word_len):
+                word = s[j:j + word_len]
+                if word_dict.get(word, 0) != 0:
+                    word_dict[word] -= 1
+                    queue.append(word)
+                    if sum(word_dict.values()) == 0:
+                        result.append(j - all_word_len + word_len)
+                        last_element = queue.popleft()
+                        word_dict[last_element] = word_dict.get(last_element, 0) + 1
+                else:
+                    while len(queue):
+                        last_element = queue.popleft()
+                        if last_element == word:
+                            queue.append(word)
+                            break
+                        else:
+                            word_dict[last_element] = word_dict.get(last_element, 0) + 1
+                            if word_dict[last_element] > ori_word_dict[last_element]:
+                                word_dict = ori_word_dict.copy()
+
         return result
