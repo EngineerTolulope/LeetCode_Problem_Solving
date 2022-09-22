372cec3d0df0d29926227c8acc2df510ec884fda
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-22 00:04:54 -0400

    Time: 100 ms (5.24%), Space: 13.9 MB (28.77%) - LeetHub

--- 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
@@ -9,7 +9,7 @@ class Solution:
         
         found_states = {}
         for i in range(n):
-            cells_string = tuple(cells)
+            cells_string = str(cells)
             if cells_string in found_states:
                 length_loop = i - found_states[cells_string]
                 return self.prisonAfterNDays(cells, (n - i) % length_loop)
