500c8e2ca193f9edaf4686cc406a40bb1b745afe
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-21 20:14:35 -0400

    Time: 477 ms (16.77%), Space: 14.9 MB (12.88%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def depth_first_search(start):
+            visited.add(start)
+            for end in range(len(isConnected)):
+                if isConnected[start][end] and end not in visited:
+                    depth_first_search(end)
+            
+        
+        
+        visited, province_count = set(), 0
+        for start in range(len(isConnected)):
+            if start not in visited:
+                province_count += 1
+                depth_first_search(start)
+        return province_count
