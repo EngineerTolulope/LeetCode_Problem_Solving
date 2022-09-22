318ef57ecd8e3ebb481ef25cc35646f1aac61210
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-22 12:18:43 -0400

    Time: 1249 ms (95.36%), Space: 91.9 MB (31.39%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -9,5 +9,4 @@ class Solution:
         for i in range(2, int(n ** 0.5) + 1):
             if primes[i]:
                 primes[i * i:n:i] = [0] * len(primes[i * i:n:i])
-                # print(primes)
         return sum(primes)
