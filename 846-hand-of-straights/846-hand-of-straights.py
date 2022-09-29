89499ee976124679dcc0f613a361960ea03bb4aa
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-09-29 13:33:56 -0400

    Time: 507 ms (27.12%), Space: 15.7 MB (45.80%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+        
+        num_count = {}
+        for num in hand:
+            num_count[num] = num_count.get(num, 0) + 1
+        
+        min_heap = list(num_count.keys())
+        heapq.heapify(min_heap)
+        while min_heap:
+            num = min_heap[0]
+            for n in range(num, num + groupSize):
+                if n not in num_count:
+                    return False
+                num_count[n] -= 1
+                if num_count[n] == 0:
+                    if n != min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        return True
+                
