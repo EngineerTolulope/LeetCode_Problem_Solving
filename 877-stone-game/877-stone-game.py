630d31b39182307f988eff49a4a091cb8d64102c
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-10-01 13:08:54 -0400

    Time: 2104 ms (5.02%), Space: 125.8 MB (9.14%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,3 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        dp = {} # (left, right) -> max alice total
+        
+        def depth_first_search(left, right):
+            if left > right:
+                return 0
+            if (left, right) in dp:
+                return dp[(left, right)]
+        
+            alice_turn = True if (right - left + 1) % 2 == 0 else False 
+            left_num = piles[left] if alice_turn else 0
+            right_num = piles[right] if alice_turn else 0
+            dp[(left, right)] = max(depth_first_search(left + 1, right) + left_num,
+                                    depth_first_search(left, right - 1) + right_num)
+            
+            return dp[(left, right)]
+        return depth_first_search(0, len(piles) - 1) #> (sum(piles)) // 2
