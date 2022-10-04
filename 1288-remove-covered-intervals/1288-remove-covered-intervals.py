93713138f9bb3e00e14fc89d5d90afc92f06f17f
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-10-04 11:37:43 -0400

    Time: 184 ms (44.36%), Space: 14.3 MB (91.10%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        result = [intervals[0]]
+        for left, right in intervals[1:]:
+            prev_left, prev_right = result[-1]
+            if left >= prev_left and right <= prev_right:
+                continue
+            result.append([left, right])
+        return len(result)
+            
