7061cc8fd39cb7144cb275bd915d854c9e0cfc68
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-10-05 15:11:01 -0400

    Time: 5042 ms (37.85%), Space: 13.9 MB (89.46%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = [num**2 for num in range(0, n) if num**2 <= n]
+        dp = [n] * (n + 1)
+        dp[0] = 0
+        
+        for target in range(1, n + 1):
+            dp[target] = target
+            for square in squares:
+                if target - square < 0:
+                    break
+                dp[target] = min(dp[target], 1 + dp[target - square])
+        return dp[n]
