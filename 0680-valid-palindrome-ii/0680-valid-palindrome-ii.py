e703621d801ca85f0effa877b0e55bed3722b63d
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-10-30 12:23:37 -0400

    Time: 393 ms (34.33%), Space: 14.5 MB (48.14%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         def valid_normal_palindrome(left, right):
-            while left <= right:
+            while left < right:
                 if s[left] == s[right]:
                     left += 1
                     right -= 1
@@ -11,7 +11,7 @@ class Solution:
         
         
         left, right = 0, len(s) - 1
-        while left <= right:
+        while left < right:
             if s[left] != s[right]:
                 skip_left = valid_normal_palindrome(left + 1, right)
                 skip_right = valid_normal_palindrome(left, right - 1)
