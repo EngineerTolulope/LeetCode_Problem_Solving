694af20912a27557c859766d9ba961ac2d70ef90
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-11-01 17:13:40 -0400

    Time: 1937 ms (9.28%), Space: 18.9 MB (26.42%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,24 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ROWS, COLUMNS = len(grid), len(grid[0])
+        def dfs(row, column):
+            if (row < 0 or column < 0 or row == ROWS or column == COLUMNS
+               or grid[row][column] == 0):
+                return 1
+            if grid[row][column] == '*':
+                return 0
+            
+            grid[row][column] = '*'
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            result = 0
+            for dx, dy in directions:
+                new_row = row + dx
+                new_column = column + dy
+                result += dfs(new_row, new_column)
+            return result
+        
+        
+        for row in range(ROWS):
+            for column in range(COLUMNS):
+                if grid[row][column]:
+                    return dfs(row, column)
