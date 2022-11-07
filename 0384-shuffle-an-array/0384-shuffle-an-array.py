1b029d1206a70c18dc15d52f1aa7427677a94d93
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2022-11-07 10:05:10 -0500

    Time: 603 ms (5.07%), Space: 17.7 MB (22.47%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -7,7 +7,7 @@ class Solution:
         return self.nums
 
     def shuffle(self) -> List[int]:
-        new_nums = self.nums[:]
+        new_nums = self.nums.copy()
         nums_len = len(new_nums)
         for i in range(nums_len):
             j = randint(i, nums_len - 1)
