12fbb2cc25cc765c8284e84cbcce5ce0f6bb9083
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-01-05 15:03:57 -0500

    Time: 904 ms (46.59%), Space: 25.3 MB (87.78%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        close, max_close = 0, 0
+        
+        for bracket in s:
+            if bracket == '[':
+                close -= 1
+            elif bracket == ']':
+                close += 1
+            max_close = max(close, max_close)
+        return (max_close + 1) // 2
+            
