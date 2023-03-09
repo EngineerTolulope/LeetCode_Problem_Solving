51d00a95d2fe303073333e9151121ee5563fbf5a
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-03-09 18:49:56 -0500

    Time: 74 ms (37.46%), Space: 14.1 MB (45.46%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_index = {num:i for i, num in enumerate(nums1)}
+        mono_stack = []
+        result = [-1] * len(nums1)
+        
+        for num in nums2:
+            while mono_stack and mono_stack[-1] < num:
+                prev_num = mono_stack.pop()
+                index = nums1_index[prev_num]
+                result[index] = num
+            
+            if num in nums1:
+                mono_stack.append(num)
+        return result
