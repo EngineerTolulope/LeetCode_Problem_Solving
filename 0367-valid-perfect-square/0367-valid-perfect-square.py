d81aa21bf445bb023906b863d061c241278340e5
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-16 12:30:24 -0400

    Time: 56 ms (7.21%), Space: 16.4 MB (23.08%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -3,14 +3,17 @@ class Solution:
         if num == 1:
             return True
         
-        left, right = 1, (num // 2)
+        left, right = 1, num // 2
+        
         while left <= right:
-            middle = left + (right - left) // 2
+            middle = (left + right) // 2
             square = middle * middle
-            if square > num:
-                right = middle - 1
+            
+            if square == num:
+                return True
             elif square < num:
                 left = middle + 1
             else:
-                return True
+                right = middle - 1
+        
         return False
