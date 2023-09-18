4c4bdadff51cea74cbfd244af54d2f16fe796d8f
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-18 11:35:47 -0400

    Time: 3184 ms (12.16%), Space: 120.8 MB (9.14%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.py
+++ 2035-count-sub-islands/2035-count-sub-islands.py
@@ -1,39 +1,5 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        ROWS, COLS = len(grid1), len(grid1[0])
-        visit = set()
-
-        def dfs(r, c):
-            if (
-                r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or grid2[r][c] == 0
-                or (r, c) in visit
-            ):
-                return True
-
-            visit.add((r, c))
-            res = True
-            if grid1[r][c] == 0:
-                res = False
-
-            res = dfs(r - 1, c) and res
-            res = dfs(r + 1, c) and res
-            res = dfs(r, c - 1) and res
-            res = dfs(r, c + 1) and res
-            return res
-
-        count = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid2[r][c] and (r, c) not in visit and dfs(r, c):
-                    count += 1
-        return count
-        
-        
-        
         ROWS, COLUMNS = len(grid2), len(grid2[0])
         visited = set()
 
@@ -58,8 +24,8 @@ class Solution:
         island_count = 0
         for row in range(ROWS):
             for column in range(COLUMNS):
-                if (grid2[row][column] == 1 and depth_first_search(row, column) and
-                    (row, column) not in visited):
+                if (grid2[row][column] == 1 and (row, column) not in visited 
+                                            and depth_first_search(row, column)):
                     island_count += 1
         return island_count
 
