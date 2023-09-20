9676b8e75f551db72e287a3c237e6887c6518b0c
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-19 22:37:28 -0400

    Time: 238 ms (63.16%), Space: 24.5 MB (30.5%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        dominoes = list(dominoes)
+        queue = deque()
+
+        for index, domino in enumerate(dominoes):
+            if domino != ".":
+                queue.append((index, domino))
+        
+        while queue:
+            index, domino = queue.popleft()
+            if domino == "L":
+                if index > 0 and dominoes[index - 1] == ".":
+                    dominoes[index - 1] = "L"
+                    queue.append((index - 1, "L"))
+            elif domino == "R":
+                if index + 1 < len(dominoes) and dominoes[index + 1] == ".":
+                    if index + 2 < len(dominoes) and dominoes[index + 2] == "L":
+                        queue.popleft()
+                    else:
+                        dominoes[index + 1] = "R"
+                        queue.append((index + 1, "R"))
+        return "".join(dominoes)
