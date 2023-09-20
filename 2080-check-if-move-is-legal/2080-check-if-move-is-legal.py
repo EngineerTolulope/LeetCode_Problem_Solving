d38f4816c889ea88c049324b284577d85e870941
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-20 13:45:44 -0400

    Time: 58 ms (66.91%), Space: 16.2 MB (98.88%) - LeetHub

--- 2080-check-if-move-is-legal/2080-check-if-move-is-legal.py
+++ 2080-check-if-move-is-legal/2080-check-if-move-is-legal.py
@@ -4,20 +4,27 @@ class Solution:
         directions = [(0, 1), (1, 0), (1, 1), (-1, -1), (-1, 0), (0, -1), (1, -1), (-1, 1)]
         board[rMove][cMove] = color
 
+        def is_valid_position(row, column):
+            return 0 <= row < ROWS and 0 <= column < COLUMNS
+
         def is_legal_move(row, column, color, direction):
-            row, column = row + direction[0], column + direction[1]
+            row += direction[0]
+            column += direction[1]
             total_length = 1
-            while row in range(ROWS) and column in range(COLUMNS):
+
+            while is_valid_position(row, column):
                 total_length += 1
                 if board[row][column] == ".":
                     return False
                 if board[row][column] == color:
                     return total_length >= 3
-                row, column = row + direction[0], column + direction[1]
+                row += direction[0]
+                column += direction[1]
+
             return False
 
         for direction in directions:
             if is_legal_move(rMove, cMove, color, direction):
                 return True
+
         return False
-                
