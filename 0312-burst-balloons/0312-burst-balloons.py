522be3ed7fb7874db6e27b2c90d6f14c32b7b6d0
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-20 15:03:02 -0400

    Time: 3518 ms (79.98%), Space: 18.3 MB (98.81%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,6 +1,5 @@
-from typing import List
-
 class Solution:
+    # bottom up dp approach
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         n = len(nums)
@@ -15,3 +14,26 @@ class Solution:
                     dp[left][right] = max(dp[left][right], coins)
 
         return dp[0][n - 1]
+    
+    # recursive approach
+    def maxCoins_(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        cache = {}
+
+        def depth_first_search(left, right):
+            if left > right:
+                return 0
+            if (left, right) in cache:
+                return cache[(left, right)]
+
+            max_coins = 0
+            for i in range(left, right + 1):
+                coins = nums[left - 1] * nums[i] * nums[right + 1]
+                coins += (depth_first_search(left, i - 1) 
+                          + depth_first_search(i + 1, right))
+                max_coins = max(max_coins, coins)
+            
+            cache[(left, right)] = max_coins
+            return max_coins
+
+        return depth_first_search(1, len(nums) - 2)
