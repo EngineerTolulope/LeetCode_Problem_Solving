bae27f88b2f55e2bc32e834154139cc20a27a33e
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-21 14:18:06 -0400

    Time: 177 ms (32.82%), Space: 21.2 MB (77.19%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -1,9 +1,13 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        count_gaps = {0 : 0}
+        count_gaps = defaultdict(int)
+        max_gaps = 0
+
         for row in wall:
             total = 0
             for brick in row[:-1]:
                 total += brick
-                count_gaps[total] = count_gaps.get(total, 0) + 1
-        return len(wall) - max(count_gaps.values())
+                count_gaps[total] += 1
+                max_gaps = max(max_gaps, count_gaps[total])
+
+        return len(wall) - max_gaps
