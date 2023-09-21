0de72c05f94aab8ab63a69409915f9287101fd61
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-21 11:15:22 -0400

    Time: 43 ms (76%), Space: 16.1 MB (85.32%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,3 +1,18 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+        left, right = 1, n
+        result = 0
+
+        while left <= right:
+            middle = (left + right) // 2
+            needed_coins = (middle / 2) * (middle + 1)
+            if needed_coins > n:
+                right = middle - 1
+            else:
+                result = max(result, middle)
+                left = middle + 1
+        return result
+
+    # maths solution
+    def arrangeCoins_(self, n: int) -> int:
         return math.floor(math.sqrt(2 * n + 0.25) - 0.5)
