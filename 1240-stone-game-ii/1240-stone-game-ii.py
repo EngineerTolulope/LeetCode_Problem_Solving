dccad6e9beedbf31af5402275a9dd42b7668a21c
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-25 17:47:07 -0400

    Time: 372 ms (69.18%), Space: 18.5 MB (51.96%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.py
+++ 1240-stone-game-ii/1240-stone-game-ii.py
@@ -1,5 +1,30 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        cache = {}
+
+        def depth_first_search(i, M):
+            if i >= n:
+                return 0
+            if (i, M) in cache:
+                return cache[(i, M)]
+            
+            maximum_stones = float("-inf")
+            total = 0
+            for X in range(1, 2 * M + 1):
+                if i + X > n:
+                    break
+                total += piles[i + X - 1]
+                opponent_stones = depth_first_search(i + X, max(M, X))
+                maximum_stones = max(maximum_stones, total - opponent_stones)
+            
+            cache[(i, M)] = maximum_stones
+            return maximum_stones
+
+        return (sum(piles) + depth_first_search(0, 1)) // 2
+
+    # initial 
+    def stoneGameII_(self, piles: List[int]) -> int:
         cache = {}
 
         def depth_first_search(is_alice, i, M):
