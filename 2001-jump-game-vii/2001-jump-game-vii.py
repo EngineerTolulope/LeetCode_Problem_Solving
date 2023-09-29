c9953e77e509a1b225472ac02cf08f68f4a0677a
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-09-29 12:02:25 -0400

    Time: 349 ms (37.64%), Space: 29.9 MB (5.32%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.py
+++ 2001-jump-game-vii/2001-jump-game-vii.py
@@ -1,20 +1,31 @@
+from collections import deque
+
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
         queue = deque([0])
         farthest = 0
         s_length = len(s)
+        ones = set()
+
+        for i in range(s_length):
+            if s[i] == "0":
+                ones.add(i)
 
         while queue:
             index = queue.popleft()
+
+            # Find the next valid position to jump to
             start = max(minJump + index, farthest + 1)
             end = min(maxJump + index, s_length - 1)
 
             for j in range(start, end + 1):
-                if s[j] == "0":
+                if j in ones:
                     queue.append(j)
+
                     if j == s_length - 1:
                         return True
 
+            # Update the farthest position reached
             farthest = max(farthest, end)
 
         return False
