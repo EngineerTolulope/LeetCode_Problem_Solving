2a6b9e3f3f8c6cd09485c9925cb0f010f485ac3f
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-02 13:05:14 -0400

    Time: 908 ms (90.39%), Space: 30.9 MB (64.59%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
@@ -1,5 +1,15 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
+        even_sum = 0  # Maximum sum of even indices
+        odd_sum = 0  # Maximum sum of odd indices
+
+        for num in nums:
+            even_sum, odd_sum = max(odd_sum + num, even_sum), max(even_sum - num, odd_sum)
+
+        return max(even_sum, odd_sum)
+
+    # initial
+    def maxAlternatingSum_(self, nums: List[int]) -> int:
         cache = {}
 
         def depth_first_search(index, is_even):
