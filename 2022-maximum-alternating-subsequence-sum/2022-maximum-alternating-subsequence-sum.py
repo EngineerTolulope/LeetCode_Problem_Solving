fb900b05dbd174ff3c198802f5a7518c6ecf539f
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-02 12:59:43 -0400

    Time: 2113 ms (20.07%), Space: 390.4 MB (27.15%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
@@ -2,14 +2,16 @@ class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         cache = {}
 
-        def dfs(index, is_even):
+        def depth_first_search(index, is_even):
             if index == len(nums):
                 return 0
             if (index, is_even) in cache:
                 return cache[(index, is_even)]
 
-            num = nums[index] if is_even else -1 * nums[index] 
-            result = max(num + dfs(index + 1, not is_even), dfs(index + 1, is_even))
+            num = nums[index] if is_even else -nums[index]
+            result = max(num + depth_first_search(index + 1, not is_even),
+                         depth_first_search(index + 1, is_even))
             cache[(index, is_even)] = result
             return result
-        return dfs(0, True)
+
+        return depth_first_search(0, True)
