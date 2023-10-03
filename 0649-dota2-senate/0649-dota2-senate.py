3c005fcfe5fc0131339a58e476ea89887b777245
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-03 15:32:12 -0400

    Time: 53 ms (79.98%), Space: 16.7 MB (31.09%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,22 +1,19 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        senate_list = list(senate)
-        n = len(senate_list)
-        radiant = []
-        dire = []
+        r_queue, d_queue = deque(), deque()
 
-        for i, senator in enumerate(senate_list):
+        for i, senator in enumerate(senate):
             if senator == "R":
-                radiant.append(i)
+                r_queue.append(i)
             else:
-                dire.append(i)
+                d_queue.append(i)
 
-        while radiant and dire:
-            if radiant[0] < dire[0]:
-                radiant.append(radiant[0] + n)
+        while d_queue and r_queue:
+            d_senator = d_queue.popleft()
+            r_senator = r_queue.popleft()
+            
+            if d_senator < r_senator:
+                d_queue.append(d_senator + len(senate))
             else:
-                dire.append(dire[0] + n)
-            radiant.pop(0)
-            dire.pop(0)
-
-        return "Radiant" if radiant else "Dire"
+                r_queue.append(r_senator + len(senate))
+        return "Radiant" if r_queue else "Dire"
