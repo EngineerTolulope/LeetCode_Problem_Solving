124d460e5cc2a4bf3158e63f042ddd98cf826d4f
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-05 11:15:00 -0400

    Time: 37 ms (78.61%), Space: 16.4 MB (29.11%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,25 +1,36 @@
+import random
+import string
+
 class Codec:
     def __init__(self):
-        self.encode_map, self.decode_map = {}, {}
+        self.encode_map = {}
+        self.decode_map = {}
         self.base_url = "http://tinyurl.com/"
-
+        self.characters = string.ascii_letters + string.digits
 
     def encode(self, long_url: str) -> str:
-        """Encodes a URL to a shortened URL.
-        """
-        tiny_url = self.base_url + str(len(self.encode_map) + 1)
+        """Encodes a URL to a shortened URL."""
+        if long_url in self.encode_map:
+            return self.encode_map[long_url]
+
+        tiny_url = self.generate_short_url()
         self.encode_map[long_url] = tiny_url
         self.decode_map[tiny_url] = long_url
         return tiny_url
 
-        
-
     def decode(self, short_url: str) -> str:
-        """Decodes a shortened URL to its original URL.
-        """
+        """Decodes a shortened URL to its original URL."""
+        if short_url not in self.decode_map:
+            raise ValueError("Invalid short URL")
         return self.decode_map[short_url]
-        
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
+    def generate_short_url(self) -> str:
+        """Generates a unique short URL."""
+        while True:
+            tiny_url = self.base_url + self.generate_random_string()
+            if tiny_url not in self.decode_map:
+                return tiny_url
+
+    def generate_random_string(self) -> str:
+        """Generates a random string for the short URL."""
+        return ''.join(random.choice(self.characters) for _ in range(6))
