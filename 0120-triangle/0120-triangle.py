de245ce60e5f1e73ce680dab37dd89c00c37f1f7
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-05 14:53:46 -0400

    Time: 75 ms (25.08%), Space: 17.3 MB (62.26%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,5 +1,14 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1].copy()
+
+        for row in triangle[-2::-1]:
+            for i in range(len(row)):
+                dp[i] = min(dp[i], dp[i + 1]) + row[i]
+
+        return dp[0]
+
+    def minimumTotal_(self, triangle: List[List[int]]) -> int:
         dp = [0] * (len(triangle) + 1)
 
         for row in triangle[::-1]:
