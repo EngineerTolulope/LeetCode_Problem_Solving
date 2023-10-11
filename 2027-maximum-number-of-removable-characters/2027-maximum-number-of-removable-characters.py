36d0c4e7471153f8b6a07a0d83b78ae0ecbf42d6
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-11 12:34:36 -0400

    Time: 1849 ms (89.54%), Space: 36.6 MB (26.28%) - LeetHub

---
+++ 2027-maximum-number-of-removable-characters/2027-maximum-number-of-removable-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        def is_subsequence(long_string, subsequence):
+            i, j = 0, 0
+
+            for i in range(len(long_string)):
+                if i not in removed and long_string[i] == subsequence[j]:
+                    j += 1
+                    if j == len(subsequence):
+                        break
+            return j == len(subsequence)
+
+        result = 0
+        left, right = 0, len(removable) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            removed = set(removable[:middle + 1])
+            if is_subsequence(s, p):
+                result = max(result, middle + 1)
+                left = middle + 1
+            else:
+                right = middle - 1
+        return result
