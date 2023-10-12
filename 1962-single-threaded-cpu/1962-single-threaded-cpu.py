6cf25605794ee86f6999df421070d9f1e6c6a626
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-12 13:57:32 -0400

    Time: 1603 ms (67.91%), Space: 64.1 MB (82.2%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -1,15 +1,19 @@
+import heapq
+from typing import List
+
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        for i, task in enumerate(tasks):
-            task.append(i)
-        tasks.sort(key = lambda x: x[0])
+        indexed_tasks = [(task[0], task[1], i) for i, task in enumerate(tasks)]
+        indexed_tasks.sort()
 
-        result, min_heap = [], []
-        current_time, i = tasks[0][0], 0
+        result = []
+        min_heap = []
+        current_time = indexed_tasks[0][0]
+        i = 0
 
-        while i < len(tasks) or min_heap:
-            while i < len(tasks) and current_time >= tasks[i][0]:
-                heapq.heappush(min_heap, tasks[i][1:])
+        while i < len(indexed_tasks) or min_heap:
+            while i < len(indexed_tasks) and current_time >= indexed_tasks[i][0]:
+                heapq.heappush(min_heap, (indexed_tasks[i][1], indexed_tasks[i][2]))
                 i += 1
             
             if min_heap:
@@ -17,6 +21,6 @@ class Solution:
                 result.append(index)
                 current_time += process_time
             else:
-                current_time = tasks[i][0]
+                current_time = indexed_tasks[i][0]
 
         return result
