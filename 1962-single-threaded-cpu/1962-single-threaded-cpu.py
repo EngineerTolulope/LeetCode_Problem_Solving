e48b2129c640b461b302bc75a19838d59b8e0cdd
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-12 14:01:24 -0400

    Time: 1640 ms (52.42%), Space: 65.5 MB (43.63%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -23,24 +23,24 @@ class Solution:
 
         return result
     
-    def getOrder_(self, tasks: List[List[int]]) -> List[int]:
-        for i, task in enumerate(tasks):
-            task.append(i)
-        tasks.sort(key = lambda x: x[0])
+    # def getOrder_(self, tasks: List[List[int]]) -> List[int]:
+    #     for i, task in enumerate(tasks):
+    #         task.append(i)
+    #     tasks.sort(key = lambda x: x[0])
 
-        result, min_heap = [], []
-        current_time, i = tasks[0][0], 0
+    #     result, min_heap = [], []
+    #     current_time, i = tasks[0][0], 0
 
-        while i < len(tasks) or min_heap:
-            while i < len(tasks) and current_time >= tasks[i][0]:
-                heapq.heappush(min_heap, tasks[i][1:])
-                i += 1
+    #     while i < len(tasks) or min_heap:
+    #         while i < len(tasks) and current_time >= tasks[i][0]:
+    #             heapq.heappush(min_heap, tasks[i][1:])
+    #             i += 1
             
-            if min_heap:
-                process_time, index = heapq.heappop(min_heap)
-                result.append(index)
-                current_time += process_time
-            else:
-                current_time = tasks[i][0]
+    #         if min_heap:
+    #             process_time, index = heapq.heappop(min_heap)
+    #             result.append(index)
+    #             current_time += process_time
+    #         else:
+    #             current_time = tasks[i][0]
 
-        return result
+    #     return result
