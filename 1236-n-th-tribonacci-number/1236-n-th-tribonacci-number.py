c11c2724ffa419dc88a6569d5560562517c1c3fe
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-13 15:48:21 -0400

    Time: 28 ms (96.93%), Space: 16.1 MB (96.27%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -4,6 +4,6 @@ class Solution:
         if n < 3:
             return nums[n]
 
-        for i in range(3, n + 1):
+        for _ in range(3, n + 1):
             nums[0], nums[1], nums[2] = nums[1], nums[2], sum(nums)
         return nums[-1]
