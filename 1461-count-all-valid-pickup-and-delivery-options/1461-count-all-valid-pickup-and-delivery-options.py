d121ce11f5d5aec1ca32dfa9c1f75772c1cb7aa1
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-17 11:04:16 -0400

    Time: 38 ms (62.58%), Space: 16.3 MB (67.03%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
@@ -1,5 +1,17 @@
 class Solution:
     def countOrders(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        remaining_slots = 2 * n
+        result = 1
+        
+        while remaining_slots > 0:
+            valid_choices = remaining_slots * (remaining_slots - 1) // 2
+            result = (result * valid_choices) % MOD
+            remaining_slots -= 2
+        
+        return result
+
+    def countOrders_(self, n: int) -> int:
         remaining_slots = 2 * n
         result = 1
         while remaining_slots > 0:
