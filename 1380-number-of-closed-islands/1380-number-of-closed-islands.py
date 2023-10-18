85828fa87c32e5dca7d8dd48485a41e2367f9c42
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-18 15:15:58 -0400

    Time: 128 ms (33.03%), Space: 19 MB (8.58%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -3,18 +3,19 @@ class Solution:
         ROWS, COLUMNS = len(grid), len(grid[0])
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         visited = set()
+
         def dfs(row, column):
-            if (row not in range(ROWS)) or (column not in range(COLUMNS)):
+            if not (0 <= row < ROWS and 0 <= column < COLUMNS):
                 return 0
 
             if grid[row][column] == 1 or (row, column) in visited:
                 return 1
-            
+
             visited.add((row, column))
             result = 1
             for dr, dc in directions:
-                if not dfs(row + dr, column + dc):
-                    result = 0
+                result &= dfs(row + dr, column + dc)
+
             return result
 
         result = 0
@@ -22,4 +23,5 @@ class Solution:
             for column in range(COLUMNS):
                 if grid[row][column] == 0 and (row, column) not in visited:
                     result += dfs(row, column)
+
         return result
