95a05dff77eff8836bfd026587cf7269ae662ee8
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2023-10-24 17:08:35 -0400

    Time: 919 ms (87.4%), Space: 36 MB (81.33%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -17,3 +17,18 @@ class Solution:
                 remainder_index[remainder] = index
 
         return False
+
+    def checkSubarraySum_(self, nums: List[int], k: int) -> bool:
+        remainder_index = {0 : -1}
+        total = 0
+
+        for index, num in enumerate(nums):
+            total += num
+            remainder = total % k
+
+            if remainder not in remainder_index:
+                remainder_index[remainder] = index
+            elif (index - remainder_index[remainder]) > 1:
+                return True
+        return False
+            
