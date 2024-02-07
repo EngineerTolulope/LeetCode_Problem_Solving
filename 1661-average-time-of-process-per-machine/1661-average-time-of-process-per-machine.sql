9654bb0414e6da94a02804cf48ebdcabf31dc68d
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-02-07 15:01:29 -0500

    Time: 687 ms (8.94%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+SELECT machine_id, ROUND(AVG(processing_time), 3) AS processing_time
+FROM (
+    SELECT machine_id, process_id, (MAX(CASE WHEN activity_type = 'end' THEN timestamp END) - MIN(CASE WHEN activity_type = 'start' THEN timestamp END)) AS processing_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS subquery
+GROUP BY machine_id;
