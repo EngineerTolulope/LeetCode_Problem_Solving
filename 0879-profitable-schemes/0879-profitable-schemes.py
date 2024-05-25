2cbc1061a740ad196392d20c658f046b27fd37f1
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-05-25 14:18:52 -0400

    Time: 1001 ms (80.61%), Space: 17.1 MB (94.39%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,5 +1,22 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        # dp[k][j] means the number of ways to achieve at least j profit with k members
+        dp = [[0] * (minProfit + 1) for _ in range(n + 1)]
+        dp[0][0] = 1  # One way to achieve zero profit with zero members
+
+        for g, p in zip(group, profit):
+            for members in range(n, g - 1, -1):
+                for currentProfit in range(minProfit, -1, -1):
+                    newProfit = min(minProfit, currentProfit + p)
+                    dp[members][newProfit] += dp[members - g][currentProfit]
+                    dp[members][newProfit] %= MOD
+
+        return sum(dp[members][minProfit] for members in range(n + 1)) % MOD
+    
+    
+    def profitableSchemes_(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
         modulo = 10**9 + 7
 
         dp = {}
