2abd94415256f982bb462847acaf2f6bc9141fc5
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-06-04 14:42:52 -0400

    Time: 31 ms (90.92%), Space: 16.6 MB (48.55%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,10 +1,18 @@
+from collections import Counter
+
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         balloon_count = Counter("balloon")
         text_count = Counter(text)
 
-        result = sys.maxsize
+        # Initialize result with maximum possible value
+        result = float('inf')
+
+        # Iterate through the characters in 'balloon'
         for char, count in balloon_count.items():
+            # Calculate the ratio of occurrence of the character in text to that in 'balloon'
             ratio = text_count[char] // count
+            # Update result with the minimum ratio
             result = min(ratio, result)
+        
         return result
