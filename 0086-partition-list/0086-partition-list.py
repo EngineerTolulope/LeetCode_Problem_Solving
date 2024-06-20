a1dd282b359a6e15be8ee4d95c8cdfeaaa96ee53
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-06-20 12:56:37 -0400

    Time: 40 ms (37.44%), Space: 16.6 MB (45.1%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -3,21 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        left, right = ListNode(), ListNode()
-        left_tail, right_tail = left, right
+        # Initialize two dummy nodes to start the less-than and greater-or-equal lists
+        less_than_head = ListNode(0)
+        greater_equal_head = ListNode(0)
 
+        # Initialize tails for both lists
+        less_than_tail = less_than_head
+        greater_equal_tail = greater_equal_head
+
+        # Traverse the original list
         current = head
         while current:
             if current.val < x:
-                left_tail.next = current
-                left_tail = left_tail.next
+                # Append to the less-than list
+                less_than_tail.next = current
+                less_than_tail = less_than_tail.next
             else:
-                right_tail.next = current
-                right_tail = right_tail.next
+                # Append to the greater-or-equal list
+                greater_equal_tail.next = current
+                greater_equal_tail = greater_equal_tail.next
             current = current.next
         
-        left_tail.next = right.next
-        right_tail.next = None
-        return left.next
+        # Combine the two lists
+        less_than_tail.next = greater_equal_head.next
+        greater_equal_tail.next = None  # Terminate the list
+
+        # Return the combined list, starting from the less-than list head
+        return less_than_head.next   
