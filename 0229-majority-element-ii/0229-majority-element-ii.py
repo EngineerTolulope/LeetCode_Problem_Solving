c2c9c827d2af2b6e4c41b725b3a56a56b0ef3719
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-08-06 14:07:18 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = defaultdict(int)
+
+        for num in nums:
+            count[num] += 1
+
+            if len(count) <= 2:
+                continue
+            
+            new_count = defaultdict(int)
+            for num, count in count.item():
+                if count >= 2:
+                    new_count[num] = count - 1
+            count = new_count
+
+        result = []
+        for num in count:
+            if nums.count(num) > len(nums) // 3:
+                result.append(num)
+        
+        return result
