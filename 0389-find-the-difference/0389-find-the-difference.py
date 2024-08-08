2f642a7f64a01132119b794063b4359bd93ae232
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-08-08 13:41:07 -0400

    Time: 31 ms (91.67%), Space: 16.7 MB (18.14%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,12 @@
+from collections import Counter
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_count, t_count = Counter(s), Counter(t)
+        # Count the characters in both strings
+        s_count = Counter(s)
+        t_count = Counter(t)
 
+        # Find the character that differs
         for char in t_count:
-            if (char not in s_count) or (t_count[char] > s_count[char]):
-                return char 
+            if t_count[char] > s_count[char]:
+                return char
