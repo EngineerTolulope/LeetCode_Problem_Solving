2c0f462670ac6d2acc21b228a67c0984e459ac84
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-08-17 18:44:53 -0400

    Time: 43 ms (92.88%), Space: 49.1 MB (10.67%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -1,13 +1,14 @@
 /**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
+ * Applies a function to each element in an array and returns a new array.
+ * @param {number[]} arr - The array to be mapped over.
+ * @param {Function} fn - The function to apply to each element, taking two arguments: the element and its index.
+ * @return {number[]} - A new array with the results of applying `fn` to each element.
  */
 var map = function (arr, fn) {
     const result = new Array(arr.length);
 
-    for (const i in arr){
-        result[i] = fn(arr[i], Number(i));
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = fn(arr[i], i);
     }
 
     return result;
