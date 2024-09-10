7d0facad47bacf7951ca7188a9f1d1d7c0d7fd31
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2024-09-10 10:49:57 -0400

    Time: 55 ms (43.51%), Space: 48.9 MB (67.46%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -13,3 +13,21 @@ var filter = function(arr, fn) {
     }
     return result;
 };
+
+
+
+// /**
+//  * @param {number[]} arr
+//  * @param {Function} fn
+//  * @return {number[]}
+//  */
+// var filter = function(arr, fn) {
+//     const result = [];
+
+//     for(let i = 0; i < arr.length; i++){
+//         if (fn(arr[i], i)){
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result;
+// };
