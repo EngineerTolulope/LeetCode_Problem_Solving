1902d5168bc4c6f083d950a3b5f82c562c066616
Tolulope Olugbenga <Eng.Tolulope@gmail.com>
2025-02-11 16:33:28 -0500

    Time: 224 ms (86.24%), Space: 47.4 MB (72.33%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,17 +1,22 @@
+from collections import defaultdict, deque
+from typing import List
+
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        adj = defaultdict(list) # ManagerID -> List of Employees
-        for i in range(n):
-            adj[manager[i]].append(i)
-        
-        result = 0
-        queue = deque([(headID, 0)])
+        # Step 1: Build an adjacency list (org chart)
+        org_chart = defaultdict(list)
+        for empID in range(n):
+            org_chart[manager[empID]].append(empID)
+
+        # Step 2: Perform BFS to calculate max inform time
+        queue = deque([(headID, 0)])  # (Employee ID, Accumulated Inform Time)
+        max_time = 0
 
         while queue:
-            managerID, time = queue.popleft()
-            result = max(result, time)
+            managerID, curr_time = queue.popleft()
+            max_time = max(max_time, curr_time)
 
-            for empID in adj[managerID]:
-                queue.append((empID, time + informTime[managerID]))
-        
-        return result
+            for empID in org_chart[managerID]:
+                queue.append((empID, curr_time + informTime[managerID]))
+
+        return max_time
